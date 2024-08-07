af4d6baaea78d85e2e027f1063e0c85617ebfc13
Jens de Bruijn <j.a.debruijn@outlook.com>
2024-08-07 14:23:39 +0200

    update cdsapi

--- hydromt_geb/geb.py
+++ hydromt_geb/geb.py
@@ -2093,10 +2093,18 @@ class GEBModel(GridModel):
 
         ds = xr.open_mfdataset(
             files,
-            chunks={"time": 1, "latitude": XY_CHUNKSIZE, "longitude": XY_CHUNKSIZE},
+            chunks={"valid_time": 1, "latitude": XY_CHUNKSIZE, "longitude": XY_CHUNKSIZE},
             compat="equals",  # all values and dimensions must be the same,
             combine_attrs="drop_conflicts",  # drop conflicting attributes
         ).rio.set_crs(4326)
+        
+        assert "valid_time" in ds.dims
+        assert "latitude" in ds.dims
+        assert "longitude" in ds.dims
+
+        # rename valid_time to time
+        ds = ds.rename({"valid_time": "time"})
+
         # remove first time step.
         # This is an accumulation from the previous day and thus cannot be calculated
         ds = ds.isel(time=slice(1, None))
